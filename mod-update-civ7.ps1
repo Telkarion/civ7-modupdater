d5415cb94a1d056d9b387841acb826fa182a9f3c
Seb "Ours" Brun <sebastien.gcm.brun@protonmail.com>
2025-03-14 10:29:52 +0100

    Added local mode to help with test and force a list of mods without relyng on hosted one

--- mod-update-civ7.ps1
+++ mod-update-civ7.ps1
@@ -1,16 +1,25 @@
-﻿$destinationDirectory = "$env:LOCALAPPDATA\Firaxis Games\Sid Meier's Civilization VII\Mods"
+﻿param (
+    [switch] $Local,
+    [string]$WorkingDirectory = (Get-Location).Path
+)
+
+$destinationDirectory = "$env:LOCALAPPDATA\Firaxis Games\Sid Meier's Civilization VII\Mods"
 #$destinationDirectory = "$env:HOMEDRIVE$env:HOMEPATH\test"
 $logFile = Join-Path -Path $destinationDirectory -ChildPath "mod-update-civ7.log"
-$urlsFileUrl = "https://raw.githubusercontent.com/Telkarion/civ7-modupdater/refs/heads/main/modsurls"
+$urlFilename = "modsurls"
+$urlsFileUrl = "https://raw.githubusercontent.com/Telkarion/civ7-modupdater/refs/heads/main/$urlFilename"
+
+Set-Location -Path $WorkingDirectory
 
 # Fonction pour écrire des logs avec un timestamp
-function log {
+function log
+{
     param (
         [string]$message
     )
     $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
     $logMessage = "$timestamp - $message"
-    Write-Host $logMessage 
+    Write-Host $logMessage
     Add-Content -Path $logFile -Value $logMessage
 }
 
@@ -22,15 +31,21 @@ if (-not (Test-Path $destinationDirectory)) {
     exit
 }
 
+if (-not $Local)
+{
+    Write-Host "=== ATTENTION ====" -ForegroundColor yellow -BackgroundColor red
+    Write-Host "Ce script va supprimer tout le contenu du répertoire de destination (mais le remplir avec une liste de mods civ7 juteuse :3 ) " -ForegroundColor yellow
+    Write-Host " Répertoire ciblé : $destinationDirectory" -ForegroundColor green
+    $confirmation = Read-Host "Voulez-vous continuer ? (Tapez 'Oui' pour confirmer)"
 
-Write-Host "=== ATTENTION ====" -ForegroundColor yellow -BackgroundColor red 
-Write-Host "Ce script va supprimer tout le contenu du répertoire de destination (mais le remplir avec une liste de mods civ7 juteuse :3 ) " -ForegroundColor yellow 
-Write-Host " Répertoire ciblé : $destinationDirectory" -ForegroundColor green 
-$confirmation = Read-Host "Voulez-vous continuer ? (Tapez 'Oui' pour confirmer)"
-
-if ($confirmation -ne "Oui") {
-    log "Opération annulée par l'utilisateur."
-    exit
+    if ($confirmation -ne "Oui")
+    {
+        log "Opération annulée par l'utilisateur."
+        exit
+    }
+}
+else {
+    log "Mode local actif"
 }
 
 
@@ -57,48 +72,57 @@ if (-not (Get-Module -ListAvailable -Name 7Zip4PowerShell)) {
 }
 
 # Purger le répertoire de destination
-if (Test-Path $destinationDirectory) {
+if (Test-Path $destinationDirectory)
+{
     Remove-Item $destinationDirectory\* -Recurse -Force
-} else {
+}
+else
+{
     New-Item -ItemType Directory -Path $destinationDirectory
 }
 
-# Liste des URLs des fichiers ZIP à télécharger
-$defaultZipUrls = @(
-    "https://forums.civfanatics.com/resources/tcs-improved-plot-tooltip.31859",
-    "https://forums.civfanatics.com/resources/sukritacts-simple-ui-adjustments.31860",
-    "https://forums.civfanatics.com/resources/ynamp-larger-map-tsl-continents-beta.31855",
-    "https://forums.civfanatics.com/resources/trade-lens.31886",
-    "https://forums.civfanatics.com/resources/artificially-intelligent-ai-mod.31881",
-    "https://forums.civfanatics.com/resources/detailed-tech-civic-progress.31924",
-    "https://forums.civfanatics.com/resources/leonardfactorys-policy-yield-previews.32012/"
-)
 
 # Télécharger et lire les URLs à partir du fichier texte distant
-try {
-    $urlsFilePath = Join-Path -Path $destinationDirectory -ChildPath "urls.txt"
-    Invoke-WebRequest -Uri $urlsFileUrl -OutFile $urlsFilePath
-    $zipUrls = Get-Content -Path $urlsFilePath
-    log "Liste des URLs récupérée avec succès."
-} catch {
-    log "Erreur lors de la récupération de la liste des URLs. Utilisation de la liste par défaut en repli."
-    $zipUrls = $defaultZipUrls
+
+$urlsFilePath = Join-Path -Path $destinationDirectory -ChildPath "urls.txt"
+
+if ($Local)
+{
+    Copy-Item -Path ".\$urlFilename" -Destination $urlsFilePath
+}
+else
+{
+    try
+    {
+        Invoke-WebRequest -Uri $urlsFileUrl -OutFile $urlsFilePath
+        log "Liste des URLs récupérée avec succès."
+    }
+    catch
+    {
+        log "Erreur lors de la récupération de la liste des URLs. Utilisez -local si vous voulez utiliser une liste locale."
+        Read-Host "Appuyez sur Entrée pour continuer"
+        exit
+    }
 }
 
 # Télécharger et décompresser chaque fichier ZIP
-foreach ($url in $zipUrls) {
+$zipUrls = Get-Content -Path $urlsFilePath
+foreach ($url in $zipUrls)
+{
     $downloadUrl = "$url/download"
     # Télécharger le fichier ZIP
-    $response = Invoke-WebRequest -Uri $downloadUrl -Method Head    
+    $response = Invoke-WebRequest -Uri $downloadUrl -Method Head
     $zipFileName = $response.Headers["Content-Disposition"] -split ";" | ForEach-Object { $_.Trim() } | Where-Object { $_ -like "filename=*" } | ForEach-Object { $_ -replace "filename=", "" } | ForEach-Object { $_.Trim('"') }
 
-    if (-not $zipFileName) {
+    if (-not $zipFileName)
+    {
         log "Impossible de déterminer le nom du fichier pour l'URL : $downloadUrl"
         continue
     }
 
-     # Ajouter l'extension .zip si elle n'est pas déjà présente
-    if  ( -not ( $zipFileName.EndsWith(".zip") -or $zipFileName.EndsWith(".7z"))) {
+    # Ajouter l'extension .zip si elle n'est pas déjà présente
+    if (-not ( $zipFileName.EndsWith(".zip") -or $zipFileName.EndsWith(".7z") -or $zipFileName.EndsWith(".rar")))
+    {
         $zipFileName = "$zipFileName.zip"
     }
 
@@ -108,15 +132,25 @@ foreach ($url in $zipUrls) {
     Invoke-WebRequest -Uri $downloadUrl -OutFile $zipFilePath
 
     # Décompresser le fichier ZIP dans le répertoire de destination
-    if ($zipFileName.EndsWith(".zip")) {
-        Expand-Archive -Path $zipFilePath -DestinationPath $destinationDirectory
-    } elseif ($zipFileName.EndsWith(".7z")) {
-        Expand-7Zip -ArchiveFileName $zipFilePath -TargetPath $destinationDirectory
+    switch ($zipFileName) {
+        { $_.EndsWith(".7z") } {
+            Expand-7Zip -ArchiveFileName $zipFilePath -TargetPath $destinationDirectory
+        }
+        { $_.EndsWith(".zip") } {
+            Expand-Archive -Path $zipFilePath -DestinationPath $destinationDirectory
+        }
+        { $_.EndsWith(".rar") } {
+            Write-Host "RAR not supported yet"
+        }
+        Default {
+            log "Aucun décompresseur disponible pour le fichier $_"
+        }
     }
 
     # Supprimer le fichier ZIP après décompression
-    Remove-Item -Path $zipFilePath       
+    Remove-Item -Path $zipFilePath
 }
 
 
 log "Civilization 7 paré et moddé :)"
+Read-Host "Appuyez sur Entrée pour continuer"
